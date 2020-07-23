08a69715ea26e7d1707837c0ee2f1233632b76a0
MoisesMorandini <moises-morandini@hotmail.com>
2020-07-23 14:09:14 -0300

    Added get and put transactions with validation

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceDefault: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    const balance = this.transactions.reduce((accumulator, currently) => {
+      if (currently.type === 'income') {
+        accumulator.income += currently.value;
+      } else {
+        accumulator.outcome += currently.value;
+      }
+      accumulator.total = accumulator.income - accumulator.outcome;
+      return accumulator;
+    }, balanceDefault);
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
